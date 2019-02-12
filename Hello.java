392ad6b690029e95c879722760c6c021b7324909
anie1997 <43902241+anie1997@users.noreply.github.com>
2019-02-12 12:43:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello
+{
+    public static void main(String args[]){
+         
+        int loop; 
+        final String name="Hello"; 
+         
+        loop=1; 
+        while(loop<=3){
+            System.out.println(name);
+            loop++; 
+        }
+         
+    }
+}
